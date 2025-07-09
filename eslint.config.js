cf4b75954a30db147d1046d7051d654a600e7ca8
alyssialopr <alyssia.pradon@gmail.com>
2025-07-09 15:42:32 +0200

    fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,18 @@
-import globals from "globals";
-import tseslint from "typescript-eslint";
 import js from "@eslint/js";
-import prettierConfig from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  { ignores: ['dist', 'node_modules'] },
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-    ],
-    languageOptions: {
-      files: ['**/*.{ts,tsx}'],
-      ecmaVersion: 12,
-      sourceType: "module",
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        ...globals.es2021,
-      },
-    },
+    ignores: ['dist', 'node_modules'],
+  },
+  {
     rules: {
       "no-console": "warn",
-      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
       "semi": ["error", "always"],
       "quotes": ["error", "single"],
-      "comma-dangle": ["error", "only-multiline"],
-      "arrow-parens": ["error", "always"],
-      "object-curly-spacing": ["error", "always"],
       "react/jsx-uses-react": "off",
-      "react/react-in-jsx-scope": "off"
     }
-  },
-  prettierConfig // Ajout de la config Prettier comme objet séparé
-);
+  }
+];
