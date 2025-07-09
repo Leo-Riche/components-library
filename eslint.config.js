c03355b017946a5c1a587b95e78d572232aa1fb5
alyssialopr <alyssia.pradon@gmail.com>
2025-07-09 15:24:34 +0200

    chore: downgrade @typescript-eslint packages to version 7.18.0 and remove eslint-config-airbnb-typescript

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,13 @@
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import js from "@eslint/js";
 
 export default tseslint.config(
   { ignores: ['dist', 'node_modules'] },
   {
     extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
       "prettier"
     ],
     languageOptions: {
