3fd66f816172812585935b41eb1d0c4a02600e49
alyssialopr <alyssia.pradon@gmail.com>
2025-07-09 11:52:22 +0200

    add airbnb to eslint & no conflict with prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,23 @@
-import js from "@eslint/js";
 import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import { globalIgnores } from "eslint/config";
 
-export default tseslint.config([
-  globalIgnores(["dist"]),
+export default tseslint.config(
+  { ignores: ['dist', 'node_modules'] },
   {
-    files: ["**/*.{ts,tsx}"],
     extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-    ],
+    "airbnb",
+    "prettier"
+  ],
     languageOptions: {
+      files: ['**/*.{ts,tsx}'],
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-  },
-]);
+    plugins: [
+      "prettier"
+    ],
+    rules: {
+      "prettier/prettier": "error"
+    }
+  }
+);
